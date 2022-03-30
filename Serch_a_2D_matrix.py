ce8ce03cff2e8b5c8e0ce8f5a4c7146dcec16901
Shubhansh Jain <93034177+ShubhanshJain@users.noreply.github.com>
2022-03-30 10:15:13 +0530

    Add files via upload

---
+++ Serch_a_2D_matrix.py
@@ -0,0 +1,31 @@
+#Rather than serching the entire matrix, we can have two pointers who will point the max and min value in a row.
+#By this we can determine the row in which we can find the target value. The complexity will also be log(m) + log(n)
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        rows, cols = len(matrix), len(matrix[0])
+        
+        top, bot = 0,rows -1
+        while top <= bot:
+            row = (top+bot) // 2
+            if target > matrix[row][-1]:
+                top = row + 1
+            elif target < matrix[row][0]:
+                bot = row - 1
+            else:
+                break
+                
+        if not (top <= bot):
+            return False
+        row = (top+bot) // 2
+        l, r = 0, cols -1
+        while l <= r:
+            m = (l+r) // 2
+            if target > matrix[row][m]:
+                l = m + 1
+            elif target < matrix[row][m]:
+                r = m -1
+            else:
+                return True
+        return False
+
