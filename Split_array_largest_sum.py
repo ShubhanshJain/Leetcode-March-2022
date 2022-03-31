59224a46f5a7667dd58bda964a7252045bff0c02
Shubhansh Jain <93034177+ShubhanshJain@users.noreply.github.com>
2022-03-31 16:44:42 +0530

    Add files via upload

---
+++ Split_array_largest_sum.py
@@ -0,0 +1,36 @@
+class Solution:
+    def splitArray(self, nums: List[int], m: int) -> int:
+        n = len(nums)
+        
+        start = max(nums)
+        end = sum(nums)
+        
+        while start + 1< end:
+            mid = start + (end - start) // 2
+            
+            if self.split_into_pieces(nums, mid) > m:
+                start = mid + 1
+            else:
+                end = mid
+                
+        if self.split_into_pieces(nums, start) <= m:
+            return start
+                
+        return end
+    
+    def split_into_pieces(self, nums, largest_sum):
+        
+        previous_sum = 0
+        pieces = 1
+        
+        for num in nums:
+            if previous_sum + num > largest_sum:
+                previous_sum = num
+                pieces += 1
+            else:
+                previous_sum += num
+        
+        return pieces
+
+        # Time complexity - O(Nlog(N))
+        # Space complexity - O(1)
